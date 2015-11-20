cde3429eec54696125929e7be09c2291e00a7b2c
tiff8433 <tiffany.l.huang@gmail.com>
2015-11-20 10:33:31 -0800

    Implements prototypal queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.first = 0;
+  newQueue.last = 0;
+  newQueue.storage = {};
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val){
+  this.storage[this.last++] = val;
+};
 
+queueMethods.dequeue = function(){
+  var holder = this.storage[this.first];
+  if( this.size() ){
+    delete this.storage[this.first];
+    this.first++;
+  }
+  return holder;
+};
+
+queueMethods.size = function(){
+  return this.last - this.first;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.count = 0;
+
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(val){
+  this.storage[this.count++] = val;
+};
+
+stackMethods.pop = function(){
+  if( this.size() ){
+    this.count--;
+    var holder = this.storage[this.count];
+    delete this.storage[this.count];
+    return holder;
+  }
+};
+
+stackMethods.size = function(){
+  return this.count;
+};
 
 
